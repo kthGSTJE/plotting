7dd910877ea4584636652914b281e24473c36573
KTHgstje <gstje@ug.kth.se>
2026-09-22 14:31:54 +0200

    Remove commented test code and reorganize file handling for clarity

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <time.h>
-//test
 
 
 #define COMMAND_LENGTH 255
@@ -33,15 +32,26 @@ int main(void) {
             .style = "with lines"
         }
     };
-
+    FILE *testFile = NULL;
     FILE *gnupipe = NULL;
     gnupipe = _popen("gnuplot -persistent", "w");
 
+    for (int i = 0; i < 10; i++) {
+        testFile = fopen("test.txt", "w");
+        fprintf(testFile, "%d %d\n", i, i * 2);
+        fclose(testFile);
+        fprintf(gnupipe, "plot 'test.txt' with lines title 'Test Data'\n");
+    }
+    
+
     //Execute gnucommands
+    /*
     fprintf(gnupipe, "%s\n", plotSettings.title);
     fprintf(gnupipe, "%s\n", plotSettings.xLabel);
     fprintf(gnupipe, "%s\n", plotSettings.yLabel);
     fprintf(gnupipe, "plot '%s' %s title '%s'\n", plotSettings.plot.fileName, plotSettings.plot.style, plotSettings.plot.title);
+    */
+
 
     return 0;
 }
